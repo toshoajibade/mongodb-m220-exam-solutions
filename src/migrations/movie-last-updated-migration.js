c146191e4387c7092673b1e7c450524440de989f
Tosho <toshoajibade@gmail.com>
2019-03-06 11:27:13 +0100

    first commit

---
+++ src/migrations/movie-last-updated-migration.js
@@ -0,0 +1,98 @@
+const MongoClient = require("mongodb").MongoClient
+const ObjectId = require("mongodb").ObjectId
+const MongoError = require("mongodb").MongoError
+
+/**
+ * Ticket: Migration
+ *
+ * Update all the documents in the `movies` collection, such that the
+ * "lastupdated" field is stored as an ISODate() rather than a string.
+ *
+ * The Date.parse() method build into Javascript will prove very useful here!
+ * Refer to http://mongodb.github.io/node-mongodb-native/3.1/tutorials/crud/#bulkwrite
+ */
+
+// This leading semicolon (;) is to make this Immediately Invoked Function Expression (IIFE).
+// To read more about this type of expression, refer to https://developer.mozilla.org/en-US/docs/Glossary/IIFE
+;(async () => {
+  try {
+    // ensure you update your host information below!
+    const host =
+      "mongodb+srv://m220student:m220password@mflix-pn8rj.mongodb.net/test?retryWrites=true"
+    const client = await MongoClient.connect(
+      host,
+      { useNewUrlParser: true },
+    )
+    const mflix = client.db("mflix")
+    console.log(" i see you")
+    // TODO: Create the proper predicate and projection
+    // add a predicate that checks that the `lastupdated` field exists, and then
+    // check that its type is a string
+    // a projection is not required, but may help reduce the amount of data sent
+    // over the wire!
+    const predicate = { lastupdated: { $exists: true, $type: "string" } }
+    const projection = { lastupdated: 1 }
+    const cursor = await mflix
+      .collection("movies")
+      .find(predicate)
+      .project(projection)
+      .toArray()
+    const moviesToMigrate = cursor.map(({ _id, lastupdated }) => ({
+      updateOne: {
+        filter: { _id: ObjectId(_id) },
+        update: {
+          $set: {
+            lastupdated: new Date(lastupdated)
+          },
+        },
+      },
+    }))
+    let col = mflix.collection("movies");
+    await col.bulkWrite(moviesToMigrate)
+    console.log(moviesToMigrate)
+    console.log("I got here")
+    console.log(
+      "\x1b[32m",
+      `Found ${moviesToMigrate.length} documents to update`,
+    )
+    // TODO: Complete the BulkWrite statement below
+    // const { modifiedCount } = await mflix.bulkWrite(
+    //   [
+    //     { insertOne: { document: { a: 1 } } },
+    //     {
+    //       updateOne: {
+    //         filter: { a: 2 },
+    //         update: { $set: { a: 2 } },
+    //         upsert: true,
+    //       },
+    //     },
+    //     {
+    //       updateMany: {
+    //         filter: { a: 2 },
+    //         update: { $set: { a: 2 } },
+    //         upsert: true,
+    //       },
+    //     },
+    //     { deleteOne: { filter: { c: 1 } } },
+    //     { deleteMany: { filter: { c: 1 } } },
+    //     {
+    //       replaceOne: { filter: { c: 3 }, replacement: { c: 4 }, upsert: true },
+    //     },
+    //   ],
+    //   { ordered: true, w: 1 }
+    // )
+    // console.log("\x1b[32m", `${modifiedCount} documents updated`)
+    client.close()
+    process.exit(0)
+  } catch (e) {
+    if (
+      e instanceof MongoError &&
+      e.message.slice(0, "Invalid Operation".length) === "Invalid Operation"
+    ) {
+      console.log("\x1b[32m", "No documents to update")
+    } else {
+      console.error("\x1b[31m", `Error during migration, ${e}`)
+    }
+    process.exit(1)
+  }
+})()
